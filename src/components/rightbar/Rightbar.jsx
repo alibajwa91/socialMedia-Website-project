a8283800acc94cd02730c6771e637263b09a8565
Ali Bajwa <alibajwa6637@gmail.com>
2024-02-11 01:55:06 -0500

    right section to show ads and online has been created

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -1,5 +1,131 @@
 import "./rightbar.css";
 
 export default function Rightbar() {
-  return <div className="rightbar">Rightbar</div>;
+  return (
+    <div className="rightbar">
+      <div className="RightbarWrapper">
+        <div className="birthdayContainer">
+          <img src="./assets/gift.png" alt="" className="birthdayIcon" />
+          <span className="birthdaytext">
+            Today, is <b>Hassan Bajwa's </b>and your <b>3 other friend's </b>
+            birthday. Send them wishes!
+          </span>
+        </div>
+        <div className="adcontainer">
+          <img src="./assets/ad.png" alt="" className="adImg" />
+        </div>
+        <h4 className="rightbarTitle">Online friends</h4>
+        <ul className="rightbarfriendslist">
+          <li className="rightbarFriend">
+            {/* this container will add profile img with a online mark onit */}
+            <div className="rightbarProfileImgContainer">
+              <img
+                src="./assets/person/3.jpeg"
+                alt=""
+                className="rightbarProfileImg"
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarFriendName">Naina Fawad </span>
+          </li>
+          <li className="rightbarFriend">
+            {/* this container will add profile img with a online mark onit */}
+            <div className="rightbarProfileImgContainer">
+              <img
+                src="./assets/person/3.jpeg"
+                alt=""
+                className="rightbarProfileImg"
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarFriendName">Naina Fawad </span>
+          </li>{" "}
+          <li className="rightbarFriend">
+            {/* this container will add profile img with a online mark onit */}
+            <div className="rightbarProfileImgContainer">
+              <img
+                src="./assets/person/3.jpeg"
+                alt=""
+                className="rightbarProfileImg"
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarFriendName">Naina Fawad </span>
+          </li>{" "}
+          <li className="rightbarFriend">
+            {/* this container will add profile img with a online mark onit */}
+            <div className="rightbarProfileImgContainer">
+              <img
+                src="./assets/person/3.jpeg"
+                alt=""
+                className="rightbarProfileImg"
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarFriendName">Naina Fawad </span>
+          </li>{" "}
+          <li className="rightbarFriend">
+            {/* this container will add profile img with a online mark onit */}
+            <div className="rightbarProfileImgContainer">
+              <img
+                src="./assets/person/3.jpeg"
+                alt=""
+                className="rightbarProfileImg"
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarFriendName">Naina Fawad </span>
+          </li>{" "}
+          <li className="rightbarFriend">
+            {/* this container will add profile img with a online mark onit */}
+            <div className="rightbarProfileImgContainer">
+              <img
+                src="./assets/person/3.jpeg"
+                alt=""
+                className="rightbarProfileImg"
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarFriendName">Naina Fawad </span>
+          </li>{" "}
+          <li className="rightbarFriend">
+            {/* this container will add profile img with a online mark onit */}
+            <div className="rightbarProfileImgContainer">
+              <img
+                src="./assets/person/3.jpeg"
+                alt=""
+                className="rightbarProfileImg"
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarFriendName">Naina Fawad </span>
+          </li>{" "}
+          <li className="rightbarFriend">
+            {/* this container will add profile img with a online mark onit */}
+            <div className="rightbarProfileImgContainer">
+              <img
+                src="./assets/person/3.jpeg"
+                alt=""
+                className="rightbarProfileImg"
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarFriendName">Naina Fawad </span>
+          </li>{" "}
+          <li className="rightbarFriend">
+            {/* this container will add profile img with a online mark onit */}
+            <div className="rightbarProfileImgContainer">
+              <img
+                src="./assets/person/3.jpeg"
+                alt=""
+                className="rightbarProfileImg"
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarFriendName">Naina Fawad </span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
